d2850f892b82ff0e910185a9e14179e92d69dca5
EstebanVU <estebanvargasu@gmail.com>
2022-11-16 10:33:26 -0600

    mejoras tabs

--- Proyecto_dgurreck_agamboa_evargas/js/index.js
+++ Proyecto_dgurreck_agamboa_evargas/js/index.js
@@ -17,11 +17,17 @@ window.onclick = function (event) {
   }
 };
 
-function openTab(tabName) {
-  var i;
-  var x = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+// Para el control de Tabs
+function openTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
   document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
